b3744144de4c88792f955ef8d9246a85a23fa224
Ryan Yim <yknyim@gmail.com>
2019-03-04 11:06:42 -0500

    adds variables for creating a tip calculator

---
+++ tipCalc.js
@@ -0,0 +1,9 @@
+
+function tipAmount(bill, service)  {
+    let price = '';
+    let foodPrice = prompt("How much is the bill?");
+    let satisfaction = prompt("Level of service?");
+    // if service === 'good' {
+        
+    // }
+}
