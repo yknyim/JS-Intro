0ece5f54fd506595389b2f57d060e63c8c1a71fd
Ryan Yim <yknyim@gmail.com>
2019-03-04 15:22:09 -0500

    adds function with arguments for starting and ending numbers

--- nums.js
+++ nums.js
@@ -1,8 +1,16 @@
 
-function printNumbers() {
-    for (i=1; i <= 10; i++) {
+// function printNumbers() {
+//     for (i=1; i <= 10; i++) {
+//         console.log(i);
+//     }
+// }
+
+function printNumbers(firstNum, secondNum) {
+    let start = Number(firstNum);
+    let end = Number(secondNum);
+    for (i = start; i <= end; i++) {
         console.log(i);
     }
 }
 
-printNumbers();
+printNumbers(1,10);
