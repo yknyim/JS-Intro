3b972193786f55903109e2aeebf7e0844a8bd5fa
Ryan Yim <yknyim@gmail.com>
2019-03-04 15:03:42 -0500

    modifies function to list out 1 - 10

--- nums.js
+++ nums.js
@@ -1,6 +1,8 @@
 
-function printNumbers {
-    for (i=0; i <= 10; i++) {
-        console.log(i)
+function printNumbers() {
+    for (i=1; i <= 10; i++) {
+        console.log(i);
     }
 }
+
+printNumbers();
