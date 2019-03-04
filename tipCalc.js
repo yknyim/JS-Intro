291d6af48bf2b9509622f8a9115bc1d962cef506
Ryan Yim <yknyim@gmail.com>
2019-03-04 11:47:00 -0500

    adds total price with tip

--- tipCalc.js
+++ tipCalc.js
@@ -1,9 +1,22 @@
 
 function tipAmount(bill, service)  {
     let price = '';
-    let foodPrice = prompt("How much is the bill?");
-    let satisfaction = prompt("Level of service?");
-    // if service === 'good' {
-        
-    // }
+    let foodPrice = Number(`${bill}`);
+    let satisfaction = `${service}`;
+    if (satisfaction === "good") {
+        price = foodPrice * .20;
+    }
+    else if (satisfaction === "fair"){
+        price = foodPrice * .15;
+    }
+    else if (satisfaction === "bad") {
+        price = foodPrice * .10;
+    }
+    return price
 }
+
+function totalAmount(bill, service) {
+    let totalPrice = tipAmount(`${bill}`, `${service}`) + bill;
+    return totalPrice
+}
+totalAmount(100,'good');
