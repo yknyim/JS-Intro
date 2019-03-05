ff8b88526ea9029f01fccdf4bb402cf7f378cee7
Ryan Yim <yknyim@gmail.com>
2019-03-05 09:42:21 -0500

    modifies function to creat top and bottom part of box

--- box.js
+++ box.js
@@ -1,4 +1,17 @@
 
 function printBox(width, height) {
-    
+    let topBox = '';
+    for (i = 0; i <= (width - 1); i++) {
+        topBox += "*"
+    }
+    console.log(topBox)
+    let middleBox = '';
+    for (x = 0; x <= (height - 3); x++) {
+        middleBox += "*";
+    }
+    console.log(middleBox);
+    let middleBox2 = middleBox;
+    let bottomBox = topBox;
+    console.log(bottomBox);
 }
+printBox(6,4)
